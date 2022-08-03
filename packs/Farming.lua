946424d5252e495b3f783c295338bdc1166ef8c6
mercobots <mercobots@gmail.com>
2022-08-03 17:29:38 +0100

    small DOC for farming script

--- packs/Farming.lua
+++ packs/Farming.lua
@@ -1,4 +1,9 @@
 -------------------------------------------------------------------------------
+---@class Farming
+-------------------------------------------------------------------------------
+local M = {}
+
+-- Dependencies
 local Console = require('Console')
 local Image = require('ImageHelper')
 local Color = require('ColorHelper')
@@ -9,35 +14,41 @@ local OTimer = require('OTimer')
 local Move = require('Move')
 local Queue = require("Queue")
 
--------------------------------------------------------------------------------
-local M = {}
-
--------------------------------------------------------------------------------
-local anchor_offset = { 270, 80 }
+-- Private
 local slide_check = false
 
+-------------------------------------------------------------------------------
+--- Assign defaults values to class variables
+---
+---@return void
 -------------------------------------------------------------------------------
 function M:set()
     self.crop = GV.PRODUCTS[1]
     self.crop_growing = OTimer:new(self.crop.produce_time)
     self.MoveFields = Move:new(8, 10, GV.FIELD_SIZE, 25)
-
 end
 
 -------------------------------------------------------------------------------
+--- Start farming script
+---
+---@return void
+-------------------------------------------------------------------------------
 function M:start()
 
+    -- crop timeout
     if self.crop_growing:isRunning() then
         Console:show(self.crop.title .. ' - ' .. self.crop_growing:timeLeft())
         return true
     end
 
+    -- get field location and field status
     local field, tool, tool_type = self:getField()
 
+    -- move across the field
     if field then
-
         self.MoveFields:start({ tool.obj, field.obj }, 'up_left', 'up_right')
 
+        -- check silo capacity
         if self:siloFull() then
             return true
         end
@@ -45,31 +56,23 @@ function M:start()
 end
 
 -------------------------------------------------------------------------------
---[[
-function M:getAnchor()
-    if Image:R(GV.REG.align):bulkSearch(holder_list, 0, 0.75, anchor_offset) then
-        local anchor = Image:getData('target')
-        local screen = Region(0, 0, GV.SETTINGS.WIDTH, GV.SETTINGS.HEIGHT)
-        if luall.location_in_region(anchor.obj, screen) then
-            return anchor
-        end
-    end
-    return false
-end
-]]
-
+--- search for holder image and calculates his offset to match field location
+---
+---@return table,table,number @returns field __{x,y,obj}__, tool __{x,y,obj}__ and tool type (1 = crop, 2 = scythe), returns false if no field found
 -------------------------------------------------------------------------------
 function M:getField()
+    Console:show("Search Field")
     --
-    local anchor = botl.getAnchor(0, anchor_offset)
-
-    if not anchor then
-        scriptExit("Anchor not found")
+    local holder = botl.getHolder(0, { 270, 80 })
+    if not holder then
+        Console:show("Holder not found")
+        return false
     end
-    --
-    local field = botl.getAnchorClickLocation(anchor, 1, 1)
+    -- get offset starting location
+    local field = botl.getAnchorClickLocation(holder, 1, 1)
     if not field then
-        scriptExit("field not found")
+        Console:show("Field not found")
+        return false
     end
 
     local tool, tool_type = self:getFieldStatus(field)
@@ -78,21 +81,27 @@ function M:getField()
         return field, tool, tool_type
     end
 
+    Console:show("Field status undefined")
     return false
 end
 
 
 -------------------------------------------------------------------------------
+--- Check if crops are growing, if is empty or crops are ready to harvest
+---
+---@param field table @{x,y,obj}
+---@return table,table,number @returns field __{x,y,obj}__, tool __{x,y,obj}__ and tool type (1 = crop, 2 = scythe), returns false if no field found
+-------------------------------------------------------------------------------
 function M:getFieldStatus(field)
 
-    -- set small region across field location
+    Console:show("Click Field")
     --
     click(field.obj)
+    wait(0.2)
 
     -- create small region to catch ongoing img
     local x, y, w, h = field.x + 70, field.y + 50, 230, 90
     local r = Region(x, y, w, h)
-
     if Image:R(r):exists(Pattern('ongoing.png'):similar(0.8), 1) then
         Console:show(self.crop.title .. ' growing')
         return false, 0
@@ -105,9 +114,11 @@ function M:getFieldStatus(field)
     r = Region(x, y, w + 100, h + 100)
     --
     if Image:R(r):exists(Pattern('btn/switch.png'):targetOffset(self.crop.offset_x, self.crop.offset_y), 1) then
+        Console:show("Field is empty")
         -- save data target to avoid to mix whit switchSlides images
         local btn_switch = Image:getData('target')
-        --
+
+        -- set slide page for crop
         if not slide_check then
             slide_check = true
             botl.switchSlides(self.crop.slide)
@@ -116,14 +127,20 @@ function M:getFieldStatus(field)
         self.crop_growing:reset()
         return btn_switch, 1
     end
-    --
+
+    -- Ready to harvest
     if Image:R(r):exists(Pattern('farming/scythe.png'):mask():similar(0.8), 0) then
+        Console:show("Crop ready")
         return Image:getData('center'), 2
     end
 
     return false, 0
 end
 
+-------------------------------------------------------------------------------
+--- Check if silo is full or not
+---
+---@return boolean
 -------------------------------------------------------------------------------
 function M:siloFull()
     if not botl.isHomeScreen(0) and Color:exists(GV.OBJ.farming_silo_full, 3) then
@@ -145,28 +162,6 @@ function M:siloFull()
     end
     return false
 end
-
-
-
-
--------------------------------------------------------------------------------
---[[
-function M:getFieldLocation(anchor)
-    Console:show('Get Field')
-    local pos_x = math.abs(self.config.start_left - self.config.start_bottom) * _field_size
-    local pos_y = -math.abs((self.config.start_left + self.config.start_bottom) - 1) * (_field_size / 2)
-    --  change direction to left or rigth
-    pos_x = self.config.start_left > self.config.start_bottom and -pos_x or pos_x
-
-    -- set new targetOffset
-    holder.match:setTargetOffset(_holder_offset.x + pos_x, _holder_offset.y + pos_y)
-
-    -- update holder data
-    Image:updateData(holder)
-
-    return Image:getData('target')
-end
-]]
 
 return M
 
